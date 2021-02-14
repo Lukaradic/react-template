fb54693ef0553a65ea16a95e46800f80c0a40642
Luka Radic <luka.radic@imrepublic.com>
2021-02-14 01:41:14 +0100

    added build mode to compile

--- webpack.config.js
+++ webpack.config.js
@@ -2,9 +2,10 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: "development",
+    entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        path: path.resolve(__dirname, '/dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js',
     },
     module: {
